12fdbbccdcfc2bbe6a6bcc34637323ea697273df
licetus <licetus0316@foxmail.com>
2017-01-09 16:07:11 +0800

    add NavMenu.vue

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <template>
 	<div id="app">
-		<img src="./assets/logo.png">
+		<NavMenu></NavMenu>
+		<!-- <img src="./assets/logo.png">
 		<h1>{{ msg }}</h1>
 		<h2>Essential Links</h2>
 		<ul>
@@ -15,23 +16,26 @@
 			<li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
 			<li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
 			<li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
-		</ul>
+		</ul> -->
 	</div>
 </template>
 
 <script>
+import NavMenu from './components/NavMenu.vue'
 export default {
 	name: 'app',
 	data () {
 		return {
-			msg: 'Welcome to Your Vue.js App'
 		}
+	},
+	components: {
+		NavMenu
 	}
 }
 </script>
 
 <style>
-#app {
+/*#app {
 	font-family: 'Avenir', Helvetica, Arial, sans-serif;
 	-webkit-font-smoothing: antialiased;
 	-moz-osx-font-smoothing: grayscale;
@@ -56,5 +60,5 @@ li {
 
 a {
 	color: #42b983;
-}
+}*/
 </style>
---
+++ src/components/NavMenu.vue
@@ -0,0 +1,55 @@
+<template lang="html">
+	<el-row class="tac">
+		<el-col :span="3">
+		<el-menu mode="vertical" default-active="1-4-1" class="el-menu-vertical-demo" @open="handleOpen" @close="handleClose">
+			<el-submenu index="1">
+				<template slot="title"><i class="el-icon-edit"></i>用户账户管理</template>
+				<el-menu-item-group>
+					<template slot="title">分组一</template>
+					<el-menu-item index="1-1">选项1</el-menu-item>
+					<el-menu-item index="1-2">选项2</el-menu-item>
+				</el-menu-item-group>
+				<el-menu-item-group title="分组2">
+					<el-menu-item index="1-3">选项3</el-menu-item>
+				</el-menu-item-group>
+				<el-submenu index="1-4">
+					<template slot="title">选项4</template>
+					<el-menu-item index="1-4-1">选项1</el-menu-item>
+				</el-submenu>
+			</el-submenu>
+			<el-submenu index="2">
+				<template slot="title"><i class="el-icon-menu"></i>投资项目管理</template>
+				<el-menu-item-group>
+					<template slot="title">分组一</template>
+					<el-menu-item index="2-1">选项1</el-menu-item>
+					<el-menu-item index="2-2">选项2</el-menu-item>
+				</el-menu-item-group>
+				<el-menu-item-group title="分组2">
+					<el-menu-item index="2-3">选项3</el-menu-item>
+				</el-menu-item-group>
+				<el-submenu index="2-4">
+					<template slot="title">选项4</template>
+					<el-menu-item index="2-4-1">选项1</el-menu-item>
+				</el-submenu>
+			</el-submenu>
+			<el-menu-item index="3"><i class="el-icon-setting"></i>管理端设置</el-menu-item>
+		</el-menu>
+		</el-col>
+	</el-row>
+</template>
+
+<script>
+	export default {
+		methods: {
+			handleOpen(key, keyPath) {
+				console.log(key, keyPath);
+			},
+			handleClose(key, keyPath) {
+				console.log(key, keyPath);
+			}
+		}
+	}
+</script>
+
+<style lang="css">
+</style>
