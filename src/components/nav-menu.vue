8e3c9612b820d677e178d943b4a2b3d5f9b2f1d2
licetus <licetus0316@foxmail.com>
2017-01-12 05:32:05 +0800

    set theme/index.css

--- src/components/nav-menu.vue
+++ src/components/nav-menu.vue
@@ -1,40 +1,40 @@
 <template lang="html">
 	<el-row class="panel">
-		<el-col class="nav-menu-top">
+		<el-col class="panel-top">
 			<el-menu theme="dark" default-active="1" class="nav-menu-top" mode="horizontal" @select="handleSelect">
-				<el-row type="flex" class="title" justify="space-between">
-					<el-row type="flex" class="row-head">
-						<el-col :span="3">
-							<el-menu-item index="0" class="title">后台管理系统</el-menu-item>
-						</el-col>
-					</el-row>
-					<el-row type="flex" class="row-end">
-						<el-menu-item index="1">控制台</el-menu-item>
-						<el-menu-item index="2">操作指南</el-menu-item>
-						<el-submenu index="3">
-							<template slot="title">其他</template>
-							<el-menu-item index="3-1">在线帮助</el-menu-item>
-							<el-menu-item index="3-2">错误提交</el-menu-item>
-							<el-menu-item index="3-3">关于我们</el-menu-item>
-						</el-submenu>
-						<el-menu-item index="4">登录/登出</el-menu-item>
-					</el-row>
+				<el-row type="flex" class="row-end" justify="end">
+					<el-menu-item index="1">控制台</el-menu-item>
+					<el-menu-item index="2">操作指南</el-menu-item>
+					<el-submenu index="3">
+						<template slot="title">其他</template>
+						<el-menu-item index="3-1">在线帮助</el-menu-item>
+						<el-menu-item index="3-2">错误提交</el-menu-item>
+						<el-menu-item index="3-3">关于我们</el-menu-item>
+					</el-submenu>
+					<el-menu-item index="4">登录/登出</el-menu-item>
 				</el-row>
 			</el-menu>
 		</el-col>
-		<el-col :span="3" class="nav-menu-side">
-			<el-menu mode="vertical" theme="dark" class="nav-menu-side" default-active="/Dashboard"
-			@open="handleOpen" @close="handleClose" @select="handleSelect" router>
-				<template v-for="(item,index) in $router.options.routes" v-if="!item.hidden">
-					<el-submenu :index="index+''" v-if="!item.leaf">
-						<template slot="title"><i :class="item.iconCls"></i>{{item.name}}</template>
-						<el-menu-item v-for="child in item.children" :index="child.path">{{child.name}}</el-menu-item>
-					</el-submenu>
-					<el-menu-item v-if="item.leaf&&item.children.length>0" :index="item.children[0].path">
-						<i :class="item.iconCls"></i>{{item.children[0].name}}
-					</el-menu-item>
-				</template>
-			</el-menu>
+		<el-col class="panel-body">
+			<el-col class="panel-side">
+				<el-menu mode="vertical" theme="dark" class="nav-menu-side" default-active="/dashboard"
+				@open="handleOpen" @close="handleClose" @select="handleSelect" router>
+					<el-row class="title">
+						<p class="text">后台管理系统</p>
+					</el-row>
+					<template v-for="(item,index) in $router.options.routes" v-if="!item.hidden">
+						<el-submenu :index="index+''" v-if="!item.leaf">
+							<template slot="title"><i :class="item.iconCls"></i>{{item.name}}</template>
+							<el-menu-item v-for="child in item.children" :index="child.path">{{child.name}}</el-menu-item>
+						</el-submenu>
+						<el-menu-item v-if="item.leaf&&item.children.length>0" :index="item.children[0].path">
+							<i :class="item.iconCls"></i>{{item.children[0].name}}
+						</el-menu-item>
+					</template>
+				</el-menu>
+			</el-col>
+			<el-col class="panel-center">
+			</el-col>
 		</el-col>
 	</el-row>
 </template>
@@ -55,5 +55,42 @@ export default {
 }
 </script>
 
-<style lang="css">
+<style scoped>
+
+	.panel {
+		position: absolute;
+		top: 0px;
+		bottom: 0px;
+		left: 0px;
+		width: 100%;
+	}
+	.panel-top {
+		position: absolute;
+		top: 0px;
+		right: 0px;
+		left: 0px;
+		height: 60px;
+		background-color: #363d42;
+	}
+	.panel-side {
+		position: absolute;
+		top: 0px;
+		bottom: 0px;
+		left: 0px;
+		width: 200px;
+		background-color: #363d42;
+	}
+
+	.title {
+		top: 0px;
+		height: 60px;
+		font-size: 18px;
+		color: #c0ccda;
+		margin: 0 0 0 35px;
+	}
+	.text {
+		vertical-align: middle;
+	}
+
+
 </style>
